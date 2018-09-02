0562e3277cf7e4b91d482edd9daaa9a69ddcb6fb
StaneCobalt <scsladek91@gmail.com>
2018-09-01 20:33:12 -0500

    Add phase 5 bootstrap format
    added navbar, footer, containers, jumbotrons, deleted a lot of conflicting css from styles, added a css file for overriding bootstrap, and much of the header tags are now loaded via script to help clean up some redundancy.

--- Phase 5/js/scripts.js
+++ Phase 5/js/scripts.js
@@ -11,56 +11,53 @@
 
 // Global Variables
 
-const presidentEmail = "amgunther1s@semo.edu";
+//var presidentEmail = "amgunther1s@semo.edu";
+var presidentEmail = "scsladek1s@semo.edu";
 
 // EVENT LISTS
 
 const fall18 = [
+	"Fall 2018:",
 	"8/27: General Meeting"
 ];
 
 const spring18 = [
-    "2/13: Bresenham's Line Algorithm for Line of Sight - Aaron Gunther",
-    "2/20: Linux Navigation - Michael Ranciglio",
-    "4/14 - 15: MegaMinerAI 21 at Missouri S&T",
-    "4/15: Andrew Smith places 5th in student bracket at MegaMiner"
+	"Spring 2018:",
+	"2/13: Bresenham's Line Algorithm for Line of Sight - Aaron Gunther",
+	"2/20: Linux Navigation - Michael Ranciglio",
+	"4/14 - 15: MegaMinerAI 21 at Missouri S&T",
+	"4/15: Andrew Smith places 5th in student bracket at MegaMiner"
 ];
 
 const fall17 = [
+	"Fall 2017:",
 	"9/12: SIGAI is created",
 	"11/11 - 12: MegaMinerAI 20 at Missouri S&T",
-    "11/12: Michael Ranciglio, Aaron Gunther, and Tatianna Reinbolt place 3rd in globals at MegaMiner"
+	"11/12: Michael Ranciglio, Aaron Gunther, and Tatianna Reinbolt place 3rd in globals at MegaMiner"
 ];
 
-/*
- * Called upon within loadEvent()
- * The ul tag will have an id of event0, event1, etc.
- * This will return the list of events to populate that ul with
-*/
-function getSemester(id){
-    switch(id){
-        case "event0":
-            return fall17;
-        case "event1":
-            return spring18;
-        case "event2":
-            return fall18;
-        default:
-            return "-";
-            break;
-    }
-}
+var eventList = [
+	fall18,
+	spring18,
+	fall17
+];
 
-// Loads the events into the ul tags on events.html
+// Loads the events into the div eventList on events.html
 
-function loadEvent(id){
-	var event = document.getElementById(id);
-	var info = getSemester(id);
+function loadEvents(){
+	var eventDiv = document.getElementById('eventList');
+	var ul;
 	var li;
-	for(let i = 0; i < info.length; i++){
-		li = document.createElement("li");
-		li.appendChild(document.createTextNode(info[i]));
-		event.appendChild(li);
+	var info;
+	for(let i = 0; i < eventList.length; i++){
+		info = eventList[i]; //current semester of events
+		ul = document.createElement("ul");
+		for(let j = 0; j < info.length; j++){
+			li = document.createElement("li");
+			li.appendChild(document.createTextNode(info[j]));
+			ul.appendChild(li);
+		}
+		eventDiv.appendChild(ul);
 	}
 }
 
@@ -68,10 +65,10 @@ function loadEvent(id){
 
 function sendMail(){
 	try{
-		var subject = encodeURI(document.getElementById("subject").value);
-		var name = encodeURI(document.getElementById("name").value);
-		var email = encodeURI(document.getElementById("email").value);
-		var msg = encodeURI(document.getElementById("msg").value);
+		var subject = encodeURI(document.forms["signUp"]["subject"].value);
+		var name = encodeURI(document.forms["signUp"]["name"].value);
+		var email = encodeURI(document.forms["signUp"]["email"].value);
+		var msg = encodeURI(document.forms["signUp"]["msg"].value);
 		var body = "Name: " + name + "%0D%0A" + "Email: " + email + "%0D%0A" + msg;
 		var mail = "mailto:scsladek1s@semo.edu?subject=" + subject + "&body=" + body;
 		window.location.href = mail;
